b24fe70c9c6c2f5761f590e8e8d0029b5fc0f8e7
filmycinema996886-code <filmycinema996886@gmail.com>
2025-08-03 02:16:41 +0530

    Updated HeroCarousel.tsx

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -1,20 +1,17 @@
-@@ .. @@
-   const slides = [
-     {
--      image: 'https://cdn.prod.website-files.com/6364e4e0baec605ceeeff94d/684052915ba65247c394549b_Kopie%20von%20FRAMEN%20NEWS%20IMAGE%20SIZER%20-%20Creator%20(1)-p-1600.jpg',
-+      image: 'https://images.pexels.com/photos/416405/pexels-photo-416405.jpeg?auto=compress&cs=tinysrgb&w=800',
-       title: 'Back to School, Back to Business - Let\'s Get Ready for Q4 with FRAMEN',
-       buttonText: 'Explore Opportunities'
-     },
-     {
--      image: 'https://cdn.prod.website-files.com/6364e4e0baec605ceeeff94d/687e1babbdc4a3ce1f4e3267_Kopie%20von%20FRAMEN%20NEWS%20IMAGE%20SIZER%20-%20Creator.jpg',
-+      image: 'https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&cs=tinysrgb&w=800',
-       title: 'Transform Your Digital Advertising Strategy',
-       buttonText: 'Get Started'
-     },
-     {
--      image: 'https://cdn.prod.website-files.com/6364e4e0baec605ceeeff94d/686bf4f1d49ce74649ffae72_Kopie%20von%20FRAMEN%20NEWS%20IMAGE%20SIZER%20-%20Creator%20(2)-p-800.jpg',
-+      image: 'https://images.pexels.com/photos/3184338/pexels-photo-3184338.jpeg?auto=compress&cs=tinysrgb&w=800',
-       title: 'Reach Your Audience Where It Matters',
-       buttonText: 'Learn More'
-     }
+const slides = [
+    {
+      image: 'https://images.pexels.com/photos/416405/pexels-photo-416405.jpeg?auto=compress&cs=tinysrgb&w=800',
+      title: 'Back to School, Back to Business - Let\'s Get Ready for Q4 with FRAMEN',
+      buttonText: 'Explore Opportunities'
+    },
+    {
+      image: 'https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&cs=tinysrgb&w=800',
+      title: 'Transform Your Digital Advertising Strategy',
+      buttonText: 'Get Started'
+    },
+    {
+      image: 'https://images.pexels.com/photos/3184338/pexels-photo-3184338.jpeg?auto=compress&cs=tinysrgb&w=800',
+      title: 'Reach Your Audience Where It Matters',
+      buttonText: 'Learn More'
+    }
+  ];
